8b0efee5d68a717ecea3e8688c7c183033e29fc8
donovanmagryta <triops124@gmail.com>
2018-05-14 07:57:56 -0400

    Create toy.ino

---
+++ toy.ino
@@ -0,0 +1,19 @@
+#NEEDS DEBOUNCE ADDED
+
+#include <Servo.h>
+Servo myservo; //creates a servo object
+int pir = 12; //digital pin connected to the PIR's output
+void setup(){
+myservo.attach(4); //attaches servo to pin 4
+pinMode(pir, INPUT);
+}
+void loop(){
+if(digitalRead(pir) == HIGH){ 
+myservo.write(170); //to make the servo go faster, decrease the time in delays for
+delay(100000); //to make it go slower, increase the number.
+}
+if(digitalRead(pir) == LOW){
+myservo.write(0); //to make the servo go faster, decrease the time in delays for
+delay(5); //to make it go slower, increase the number.
+}
+}
